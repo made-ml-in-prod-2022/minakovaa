f47494d68619cf9d55a8ce76a5e43039fed7f57c
Alexander Minakov <minakov.alexander.a@yandex.ru>
2022-04-29 11:46:33 +0300

    Create empty datascience cokiecutter project

---
+++ ml_project/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Homework_01 ML in production',
+    author='minakovaa',
+    license='',
+)
